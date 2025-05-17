ed01f43a631bb1b9ca915e6d05902b814b2896e3
Nabil Kasimi <nkasimi@c2r8p1.1337.ma>
2025-05-17 18:09:52 +0100

    i put some  funcions togathers just to reduce line and make it clear

--- main.c
+++ main.c
@@ -1,39 +1,82 @@
 #include "minishell.h"
 
-int main()
+void unused_vars(int ac, char **av , char **env);
+char *ft_readline(void);
+void expand_input(char **input);
+void parse_and_expand(char *line, char *** splitted);
+
+
+
+
+
+
+
+
+int main(int ac , char **av, char **env)
 {
-  int i;
+
   char *line;
   char **splitted;
+  splitted = NULL;
   t_list **to_free = get_garbage_pointer();
-  t_data *tokenized;
 
+  unused_vars(ac, av, env);
   while(1)
   {
-    i = 0;
     handle_signals();
-    line = readline(">>>> ");
-    if(line && *line)
-      add_history(line);
+    line = ft_readline();
     if(line == NULL)
       return((free_memory_and_exit(*to_free),1));
     if(check_error(line))
       return((free_memory_and_exit(*to_free),1));
-    splitted = customized_split(line);
-    splitted = split_with_operators(splitted);
-    while(splitted[i])
-    {
-      splitted[i] = expand_if_possible(splitted[i]);
-      i++;
-    }
-    tokenized = make_token(splitted);
-    if(tokenized)
-    {
-      remove_quotes_from_args(splitted);
-     // print_splitted(splitted);
-    }
-    parse_tokenized(tokenized);
+    parse_and_expand(line , &splitted);
+
   }
   free_memory_and_exit(*to_free);
   return (0);
+}
+
+
+
+
+
+
+void unused_vars(int ac, char **av , char **env)
+{
+  (void ) ac;
+  (void) av;
+  (void) env;
+}
+
+char *ft_readline(void)
+{
+
+  char *line;
+  line = readline(">>>> ");
+  if(line && *line)
+    add_history(line);
+  return line;
+}
+
+void expand_input(char **input)
+{
+  int i;
+  i = 0;
+  while(input[i])
+  {
+    input[i] = expand_if_possible(input[i]);
+    i++;
+  }
+}
+
+void parse_and_expand(char *line, char ***splitted)
+{
+  *splitted = customized_split(line);
+  *splitted = split_with_operators(*splitted);
+  expand_input(*splitted);
+  if(make_token(*splitted))
+  {
+    remove_quotes_from_args(*splitted);
+    print_splitted(*splitted);
+  }
 }
