22cc381e639e1a283f9a407964cb8a267a207cec
Ahmed Benkrara <abenkrar@c2r9p1.1337.ma>
2025-06-27 10:15:41 +0100

    small fix

--- main.c
+++ main.c
@@ -7,13 +7,12 @@ int main(int ac, char **av, char **env)
   ft_init_shell_block(&sh, ac, av);
   sh.env_of_export = copy_env(env);
   sh.env_cpy = copy_env(env);
- while (1) {
+ while (1)
+  {
     handle_signals(0);
     if (!ft_readline(&sh) || parse_line(&sh))
       continue;
     execute_line(&sh);
-    if(heredoc_signal_state(0))
-      continue;
     free_memory(get_garbage_pointer(1));
     free(sh.line);
   }
