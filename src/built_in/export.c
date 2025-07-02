6debe6cb7be9dcb70c9fd284e001343d9eb3e960
Nabil Kasimi <nkasimi@c2r9p2.1337.ma>
2025-07-02 12:13:01 +0100

    just a small updata

--- src/built_in/export.c
+++ src/built_in/export.c
@@ -1,29 +1,33 @@
 #include "minishell.h"
 
-int export(t_shell_control_block *s, char **to_export)
+int	export(t_shell_control_block *s, char **to_export)
 {
-  int state = 0;
-    if(!to_export || !*to_export)
-    {
-        sort_env(s->env_of_export);
-        return 0;
-    }
-    while(*to_export)
-    {
-    if(!is_valid_var(*to_export))
-      state = 1;
-    else
-    {
-      if (find_and_update_var(s->env_of_export, *to_export));
-      else
-        s->env_of_export = add_var_to_env(s->env_of_export , *to_export);
-      if(!is_it_key_value(*to_export))
-        ;
-      else if (find_and_update_var(s->env_cpy, *to_export));
-      else
-        s->env_cpy = add_var_to_env(s->env_cpy , *to_export);
-    }
-    to_export++;
-    }
-  return state;
+	int	state;
+
+	state = 0;
+	if (!to_export || !*to_export)
+	{
+		sort_env(s->env_of_export);
+		return (0);
+	}
+	while (*to_export)
+	{
+		if (!is_valid_var(*to_export))
+			state = 1;
+		else
+		{
+			if (find_and_update_var(s->env_of_export, *to_export))
+				;
+			else
+				s->env_of_export = add_var_to_env(s->env_of_export, *to_export);
+			if (!is_it_key_value(*to_export))
+				;
+			else if (find_and_update_var(s->env_cpy, *to_export))
+				;
+			else
+				s->env_cpy = add_var_to_env(s->env_cpy, *to_export);
+		}
+		to_export++;
+	}
+	return (state);
 }
--- src/built_in/is_valid_var.c
+++ src/built_in/is_valid_var.c
@@ -1,22 +1,19 @@
 #include "minishell.h"
 
-int is_valid_var(char *str)
+int	is_valid_var(char *str)
 {
-    int i;
-    i = 0;
-    if(!ft_isalpha(str[0]) && str[0] != '_')
-    {
-        printf("export: '%s' : not a valid identifier\n", str);
-        return 0;
-    }
-    while(str[i] && str[i] != '=')
-    {
-        if(!ft_isalpha(str[i]) && str[i] != '_' && ft_isdigit(str[i]))
-        {
-            printf("export: '%s' : not a valid identifier\n", str);
-            return 0;
-        }
-        i++;
-    }
-    return 1;
+	int	i;
+
+	i = 0;
+	if (!ft_isalpha(str[0]) && str[0] != '_')
+		return ((print(2, buffering("export: `", str,
+						"' : not a valid identifier\n")), 1));
+	while (str[i] && str[i] != '=')
+	{
+		if (!ft_isalpha(str[i]) && str[i] != '_' && !ft_isdigit(str[i]))
+			return ((print(2, buffering("export: `", str,
+							"' : not a valid identifier\n")), 1));
+		i++;
+	}
+	return (1);
 }
