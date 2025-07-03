58bb365c360e5d5b660edc8693c9cce02e6ac2a6
Nabil Kasimi <nkasimi@c2r8p1.1337.ma>
2025-07-03 17:49:49 +0100

    i fix a big

--- src/execution/get_cmd_and_its_args.c
+++ src/execution/get_cmd_and_its_args.c
@@ -11,7 +11,9 @@ char	**get_cmd_and_its_args(t_shell_control_block *sh)
 	ptr = sh->tokenze;
 	while (ptr)
 	{
-		if (is_symbole(ptr->type))
+		if (ptr->type == -1)
+			;
+		else if (is_symbole(ptr->type))
 			ptr = ptr->next;
 		else if (ptr->type == WORD)
 			sh->cmd_and_args[i++] = ft_strdup(ptr->word, 1);
