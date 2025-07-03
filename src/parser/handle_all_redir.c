206b77324ab1a6c4cd00a6d31a5f8c55f3e832fd
Ahmed Benkrara <abenkrar@c2r9p1.1337.ma>
2025-07-03 15:44:59 +0100

    small fix

--- src/parser/handle_all_redir.c
+++ src/parser/handle_all_redir.c
@@ -10,11 +10,11 @@ void	handle_all_redir(t_shell_control_block *shell)
 			if (shell->tokenze->type == HEREDOC)
 				shell->in_file_name = shell->tokenze->heredoc_file_name;
 			else if (shell->tokenze->type == REDIR_IN)
-				handle_redir_in((shell->tokenze->next)->word, &(shell->in_file_name));
+				handle_redir_in((shell->tokenze->next)->word, &(shell->in_file_name), shell);
 			else if (shell->tokenze->type == REDIR_OUT)
-				handle_redir_out((shell->tokenze->next)->word, &(shell->file_name));
+				handle_redir_out((shell->tokenze->next)->word, &(shell->file_name), shell);
 			else if (shell->tokenze->type == REDIR_APPEND)
-				handle_append((shell->tokenze->next)->word, &(shell->file_name));
+				handle_append((shell->tokenze->next)->word, &(shell->file_name), shell);
 		}
 		shell->tokenze = shell->tokenze->next;
 	}
